8d3cb99dddec8a4b443881e88d33249b4a9c524d
insanniity <cleydson.cg@live.com>
2021-03-11 15:48:37 -0300

    Initial commit

---
+++ src/app/Program.java
@@ -0,0 +1,74 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		while (!chessMatch.getCheckMate()) {
+			try {
+				UI.clearScream();
+				UI.printMatch(chessMatch, captured);	
+				System.out.println();
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScream();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
+				System.out.println();
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Entre com a peca a promover (B/H/R/Q) :");
+					String type = sc.nextLine().toUpperCase();
+					
+					while(!type.equals("B") && !type.equals("H") && !type.equals("R") && !type.equals("Q")){
+						System.out.println("Valor invalido! digite novamente a peca para promover(B/H/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
+				}
+				
+			}catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
+		UI.clearScream();
+		UI.printMatch(chessMatch, captured);
+		
+	}
+	
+	
+
+}
